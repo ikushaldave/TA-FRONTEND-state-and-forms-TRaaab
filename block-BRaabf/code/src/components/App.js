ae3895545e664bd1bed0471dd02ac24cc942398b
ikushaldave <ikushaldave@gmail.com>
2021-01-28 19:22:09 +0530

    understanding state block 6 completed

---
+++ block-BRaabf/code/src/components/App.js
@@ -0,0 +1,15 @@
+import Counter from "./Counter"
+import MaxCounter from "./MaxCounter"
+
+function App () {
+  return (
+    <>
+      <h1> Counter App </h1>
+      <Counter />
+      <h1> Max-Value Counter App</h1>
+      <MaxCounter />
+    </>
+  )
+}
+
+export default App;
